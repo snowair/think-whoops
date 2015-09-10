fee2dfe4073f6b3be191825b5a0614c4b1f77c02
snowair <xcoolcc@gmail.com>
2015-09-10 21:12:47 +0800

    update

--- whoops.php
+++ whoops.php
@@ -2,6 +2,8 @@
 
 namespace Snowair;
 
+use Think\Exception;
+
 class Whoops{
     public function app_begin( &$params )
     {
@@ -30,8 +32,20 @@ class Whoops{
         $whoops->pushHandler($jsonHandler);
         $whoops->register();
         restore_error_handler();
+        spl_autoload_register(array($this,'autoload'));
+
+    }
+
+    public function autoload($class)
+    {
+        $loaders = spl_autoload_functions();
+        $last = end($loaders);
+        if ($last[0]==$this) {
+            throw new Exception('类不存在:'.$class );
+        }
     }
 }
+
 
 if (class_exists( 'Snowair\Think\Behavior\HookAgent' )) {
     \Snowair\Think\Behavior\HookAgent::add('app_begin','Snowair\\Whoops');
